0f04fb1b2b52fac9457917ba3a760901021a2e97
embu97 <embu@itu.dk>
2020-03-08 16:04:38 +0100

    metrics page is running

--- server/routers/viewRoutes.js
+++ server/routers/viewRoutes.js
@@ -8,8 +8,11 @@ const express = require('express');
 const router = express.Router();
 const viewController = require('../controllers/viewController');
 
+const prom = require('../monitoring/prometheus-util');
+
 /* Routes */
 router.get('/', viewController.mainView);
+router.get('/metrics', prom.injectMetricsRoute);
 router.get('/public', viewController.renderPublicTimeLine);
 router.get('/home', viewController.renderPrivateTimeline);
 router.get('/login', viewController.renderLoginPage);
@@ -21,5 +24,6 @@ router.post('/msg', viewController.postMessage);
 router.get('/:username', viewController.renderUserTimeline);
 router.get('/:username/follow', viewController.followButton);
 router.get('/:username/unfollow', viewController.unfollowButton);
+
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -32,12 +32,39 @@ app.use(back());
 app.use('/', viewRoutes);
 app.listen(appPort, () => console.log(`Minitwit web app server listening on port ${appPort}.`));
 
-
 /* SIMULATOR SERVER */
 const simPort = config.simulator.port;
 const simulator = express();
 
 simulator.use(express.json());
 simulator.use('/', simRouter)
+
+/**
+ * This creates the module that we created in the step before.
+ * In my case it is stored in the util folder.
+ */
+var Prometheus = require('./monitoring/prometheus-util');
+
+/**
+ * The below arguments start the counter functions
+ */
+app.use(Prometheus.requestCounters);  
+app.use(Prometheus.responseCounters);
+
+/**
+ * Enable metrics endpoint
+ */
+//Prometheus.injectMetricsRoute(app);
+
+/**
+ * Enable collection of default metrics
+ */
+Prometheus.startCollection(); 
+
+/* MONITORING */
+// simulator.get('/metrics', (req, res) => {
+// 	res.set('Content-Type', register.contentType);
+// 	res.end(register.metrics());
+// });
 
 simulator.listen(simPort, () => console.log(`Simulator API server listening on port ${simPort}.`));
