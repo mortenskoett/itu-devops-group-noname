20c2937f3b1b1544cea6550ff50e9b48d42ebb39
embu97 <embu@itu.dk>
2020-03-18 17:45:57 +0100

    npm audit fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    "indent": [2, "tab", { "SwitchCase": 1, "VariableDeclarator": 1 }],
-    "no-tabs": 0,
-}
+    "indent": ["error", "tab"],
+    "no-tabs": 0
+  }
 };
