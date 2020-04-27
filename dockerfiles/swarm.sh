38a6c5ffd759de5add46fd63545263f18822c60b
Morten Skøtt <mortenskoett@gmail.com>
2020-04-27 16:15:23 +0200

    Add login feature to swarm.sh
    Done because circleci server needs to be able to login to push freshly
    built images to Dockerhub

--- dockerfiles/swarm.sh
+++ dockerfiles/swarm.sh
@@ -14,19 +14,22 @@ WHITE='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
-push() {
+login_dockerhub() {
+    echo "Logging into Dockerhub using known credentials..."
+    docker login --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
+}
+
+push_to_dockerhub() {
     echo "Pushing docker images to Dockerhub..."
     docker-compose -f "$DEPLOY_FILE" push
 }
 
-build() {
+build_images() {
     echo "Building docker images..."
     docker-compose -f "$DEPLOY_FILE" build
-
-    push
 }
 
-deploy() {
+deploy_to_swarm() {
     echo "Deploying '$APPNAME' on '$MANAGER' using '$DEPLOY_FILE'..."
     echo -e ${RED}"Did you remember to rebuild the models?"${WHITE}
 
@@ -41,17 +44,20 @@ deploy() {
 
 case $1 in
     deploy)
-        deploy ;;
+        deploy_to_swarm ;;
     build)
-        build ;;
+        build_images ;;
     push)
-        push ;;
+        push_to_dockerhub ;;
+    login)
+        login_dockerhub ;;
     *)
         echo -e "Usage:"
         echo "arg        function"
         echo "deploy:    run or update code and deploy to swarm"
         echo "build:     build all docker images in $DEPLOY_FILE"
         echo "push:      pushes images to Dockerhub"
+        echo "login:     login to Dockerhub using =\$DOCKER_USERNAME and \$DOCKER_PASSWORD"
         exit 1 ;;
 esac
 exit 0
