60f3560162c67c08a2b537259b1748a81835c3c1
Morten Skøtt <mortenskoett@gmail.com>
2020-04-27 15:59:54 +0200

    Refactor swarm.sh
    Done in order to handle needs by circleci config

--- dockerfiles/swarm.sh
+++ dockerfiles/swarm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Script to deploy to Docker Swarm.
+# Script to deploy and update code on Docker Swarm.
 
 set -eo pipefail
 
@@ -8,16 +8,50 @@ MANAGER="node-0"
 APPNAME="minitwit"
 DEPLOY_FILE="docker-stack-deploy.yml"
 
-echo "Building docker images..."
-docker-compose -f "$DEPLOY_FILE" build
+# Colors
+RED='\033[0;31m'
+WHITE='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 
-echo "Pushing docker images to Dockerhub..."
-docker-compose -f "$DEPLOY_FILE" push
+push() {
+    echo "Pushing docker images to Dockerhub..."
+    docker-compose -f "$DEPLOY_FILE" push
+}
 
-echo "Sending stack to manager node..."
-docker-machine scp "$DEPLOY_FILE" "$MANAGER":~/
+build() {
+    echo "Building docker images..."
+    docker-compose -f "$DEPLOY_FILE" build
 
-echo "Setting up applications..."
-docker-machine ssh "$MANAGER" docker stack deploy -c "$DEPLOY_FILE" "$APPNAME"
+    push
+}
 
-echo -e ${GREEN}"Applications started successfully on Docker Swarm."${WHITE}
+deploy() {
+    echo "Deploying '$APPNAME' on '$MANAGER' using '$DEPLOY_FILE'..."
+    echo -e ${RED}"Did you remember to rebuild the models?"${WHITE}
+
+    echo "Sending stack to manager node..."
+    docker-machine scp "$DEPLOY_FILE" "$MANAGER":~/
+
+    echo "Setting up applications..."
+    docker-machine ssh "$MANAGER" docker stack deploy -c "$DEPLOY_FILE" "$APPNAME"
+
+    echo -e ${GREEN}"Applications started successfully on Docker Swarm."${WHITE}
+}
+
+case $1 in
+    deploy)
+        deploy ;;
+    build)
+        build ;;
+    push)
+        push ;;
+    *)
+        echo -e "Usage:"
+        echo "arg        function"
+        echo "deploy:    run or update code and deploy to swarm"
+        echo "build:     build all docker images in $DEPLOY_FILE"
+        echo "push:      pushes images to Dockerhub"
+        exit 1 ;;
+esac
+exit 0
